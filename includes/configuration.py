714e8a04d2e3044fa126ac9c04c63cf6cc5f4c62
Guilherme <70667501+GuilhermeFiorot@users.noreply.github.com>
2022-07-20 16:38:24 +0200

    Add files via upload

---
+++ includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/formula1sg/raw"
+processed_folder_path = "/mnt/formula1sg/processed"
+presentation_folder_path = "/mnt/formula1sg/presentation"
+
+# COMMAND ----------
+
